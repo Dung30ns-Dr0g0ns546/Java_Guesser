74539775f1511762253cb5377d800d2c513b3daa
Dung30ns-Dr0g0ns546 <qaden.childs@gmail.com>
2025-03-12 13:48:30 +0000

    commit

--- JavaGuesser.java
+++ JavaGuesser.java
@@ -1,3 +1,15 @@
-import java.util.*;
+import java.util.Random;
+import java.util.Scanner;
 
-public class Guesser (
+public class Guesser {
+
+    Scanner input = new Scanner(System.in);
+    Random randomNumber = new Random();
+    int lowerBounds = 1;
+    int upperBounds = 100;
+    
+    public static void main(String[] args) {
+        new Guesser();
+    }
+
+}
