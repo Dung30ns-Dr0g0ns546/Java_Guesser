dbf905b9a51975a4107f156f9a3079046470722f
Dung30ns-Dr0g0ns546 <qaden.childs@gmail.com>
2025-04-03 21:38:47 +0000

    commit

---
+++ Guesser.java
@@ -0,0 +1,46 @@
+import java.util.Random;
+import java.util.Scanner;
+
+
+public class Guesser {
+
+
+    Scanner input = new Scanner(System.in);
+   
+    public static void main(String[] args) {
+        new Guesser();
+    }
+
+
+    public Guesser(){
+        boolean keepGoing = true;
+        while(keepGoing){
+            String result = menu();
+            if (result.equals("0")){
+                keepGoing = false;
+            } elseif (result.equals("1"));{
+                humanGuesser();
+            } else {
+                
+            }
+   
+           
+        };
+    }
+
+    public String menu(){
+        System.out.println("0. Exit");
+        System.out.println("1. Human Guesser");
+        System.out.println("2. Computer Guesser");
+
+        System.out.println();
+        System.out.println("Please enter 0-2: ");
+
+        String result = input.nextLine();
+        return result;
+    }
+    
+}
+    
+
+
